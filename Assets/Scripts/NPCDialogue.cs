aeeddd1b61f62cfc401c62ab3e584a9bbdf1dc04
Tran-DuyKhanh <160704421+Tran-DuyKhanh@users.noreply.github.com>
2025-05-17 16:21:11 +0700

    Thêm giao tiếp npc, perfabs menu

---
+++ Assets/Scripts/NPCDialogue.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class NPCDialogue : MonoBehaviour
+{
+    public GameObject dialogueUI; // UI hiển thị hội thoại
+    private bool playerInRange = false;
+    public GameObject interactHint;
+
+    void Update()
+    {
+        if (playerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            interactHint.SetActive(playerInRange);
+
+            if (playerInRange && Input.GetKeyDown(KeyCode.E))
+            {
+                dialogueUI.SetActive(!dialogueUI.activeSelf);
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+            dialogueUI.SetActive(false); // Ẩn UI khi rời khỏi
+        }
+    }
+}
